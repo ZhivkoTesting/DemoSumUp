a68a64a9ad3c740dd408b2aa773edc3bb3602d22
Zhivko Ivanov <zhivkozz85@gmail.com>
2023-02-03 18:31:56 +0200

    testData added

--- requests.js
+++ requests.js
@@ -1,16 +1,34 @@
 let axios = require('axios');
 
 let requests = function () {
-    this.getListTransactions = async function () {
-      let config = {
-    method: 'get',
-    url: 'https://api.sumup.com/v0.1/me/transactions/history',
-    headers: { 
-      'Authorization': 'Bearer sup_sk_2YJ9Q2XkQEgnDS89ulkoIumdKMV6Xe3hy', 
-    }
-  };
-        const responce = await (axios(config))
-        return JSON.stringify(responce.data.items)
-    }
+  this.getListTransactions = async function () {
+    let config = {
+      method: 'get',
+      url: 'https://api.sumup.com/v0.1/me/transactions/history',
+      headers: {
+        'Authorization': 'Bearer sup_sk_2YJ9Q2XkQEgnDS89ulkoIumdKMV6Xe3hy',
+      }
+    };
+    let request_start_at = performance.now();
+    let responce = await (axios(config))
+    let request_end_at = performance.now();
+    let request_duration = request_end_at - request_start_at;
+    console.log(request_duration);
+    return responce.data.items
+  }
+
+  this.getListBankAccaunts = async function () {
+    let config = {
+      method: 'get',
+      url: 'https://api.sumup.com/v0.1/me/merchant-profile/bank-accounts',
+      headers: {
+        'Authorization': 'Bearer sup_sk_2YJ9Q2XkQEgnDS89ulkoIumdKMV6Xe3hy',
+      }
+    };
+    const responce = await (axios(config))
+    return responce.data
+  }
+
+
 }
 module.exports = new requests()
--- tests.js
+++ tests.js
@@ -1,15 +1,15 @@
 let expect = require("chai").expect 
 let assert = require("chai").assert
 let should = require("chai").should
-var axios = require('axios');
 let requests = require("./requests.js")
-
+let testData = require ("./testData.js")
 
 describe ("API tests", function(){
 
     it('Zero user transactions response', async () => {
-        const transactions = await requests.getListTransactions()
-        assert.equal(transactions, "[]")
+        let transactions =JSON.stringify(await requests.getListTransactions()) 
+        assert.equal(transactions, testData.listOfTransactions)
     });
 
+  
 })
